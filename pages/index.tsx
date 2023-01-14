b33fa57c0bfcf9a05b7e2f818803ae2f71d85296
Anas Mazouni <madadj4@gmail.com>
2023-01-14 20:01:02 +0100

    chore: page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Email from '../components/Email';
 import Hero from '../components/Hero';
 import Socials from '../components/Socials';
@@ -6,6 +7,9 @@ import Socials from '../components/Socials';
 const Home: NextPage = () => {
   return (
     <div className="flex flex-row items-end h-full">
+      <Head>
+        <title>Anas Mazouni | Stormix</title>
+      </Head>
       <Socials />
       <Hero />
       <Email />
