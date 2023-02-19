b6e29407d6899f63f180abcea2b7b83a55925873
Anas Mazouni <madadj4@gmail.com>
2023-02-19 19:59:22 +0100

    feat: show last commit

--- src/components/atoms/WorkInProgress.tsx
+++ src/components/atoms/WorkInProgress.tsx
@@ -1,6 +1,11 @@
+import { format } from 'date-fns';
 import React from 'react';
 
+declare const __COMMIT_HASH__: string;
+declare const __COMMIT_TIMESTAMP__: string;
+
 const WorkInProgress: React.FC = () => {
+  const timestamp = format(new Date(Number(__COMMIT_TIMESTAMP__) * 1000), 'dd/MM/yyyy HH:mm:ss');
   return (
     <div className="flex items-center gap-2 px-4 py-2 dark:bg-primary-dark-800 bg-primary-light-400">
       <span>Still a work in progress. Follow me on </span>
@@ -13,7 +18,20 @@ const WorkInProgress: React.FC = () => {
       >
         @Stormix_co
       </a>
-      <span>to stay updated...</span>
+      <span className="flex-grow">to stay updated...</span>
+
+      <span>
+        Last commit -{' '}
+        <a
+          target="_blank"
+          className="text-primary"
+          href={`https://github.com/Stormix/Stormix/commit/${__COMMIT_HASH__}`}
+          rel="noreferrer"
+        >
+          {__COMMIT_HASH__}
+        </a>{' '}
+        at <b>{timestamp}</b>
+      </span>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { execSync } from 'child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString();
+const timestamp = execSync('git log -1 --format=%ct').toString();
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+    __COMMIT_TIMESTAMP__: JSON.stringify(timestamp),
+  },
   server: {
     port: 3030,
   },
