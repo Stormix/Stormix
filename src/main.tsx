2e422f05cb7d0eac85238d6b201ad4dc34bb5263
Anas Mazouni <madadj4@gmail.com>
2023-03-01 23:30:08 +0100

    fix: google tag manager

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import { BrowserRouter } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 
 import '@/i18n';
+
+TagManager.initialize({
+  gtmId: 'GTM-KW6JJTB',
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
